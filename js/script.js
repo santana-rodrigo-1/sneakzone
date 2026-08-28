7579fae0a255326aa64ec8b2b42bb0158773a8e9
RODRIGO BARBOSA DE SANTANA <santana.rodrigo@aluno.senac.rio>
2026-08-28 20:11:59 -0300

    senac

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-function comprar(nomeProduto, preco){
+function comprar(nomeProduto, preco) {
     console.log("Produto: ", nomeProduto);
     console.log("Preço: ", preco);
-
     alert(
         "Você selecionou: " + nomeProduto + " preço: R$ " + preco
     )

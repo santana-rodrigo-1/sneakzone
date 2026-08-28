b2268139254ccd9e6d22afa43b70cdde94ad7af6
RODRIGO BARBOSA DE SANTANA <santana.rodrigo@aluno.senac.rio>
2026-08-28 19:41:39 -0300

    senac

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function comprar(nomeProduto, preco){
+    console.log("Produto: ", nomeProduto);
+    console.log("Preço: ", preco);
+
+    alert(
+        "Você selecionou: " + nomeProduto + " preço: R$ " + preco
+    )
+}
